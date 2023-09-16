c383330ae9a15cdfa245409ea23438e804d90680
Carlos Mesquita <carlosmesquita94@outlook.es>
2023-09-16 20:22:25 +0200

    Create RealStateDetailsUIState

---
+++ app/src/main/kotlin/com/cmesquita/realstation/ui/details/RealStateDetailsUIState.kt
@@ -0,0 +1,12 @@
+package com.cmesquita.realstation.ui.details
+
+import com.cmesquita.realstation.ui.details.model.RealStateDetails
+
+sealed class RealStateDetailsUIState {
+
+    data object Loading : RealStateDetailsUIState()
+
+    data class Error(val message: String) : RealStateDetailsUIState()
+
+    data class Content(val realState: RealStateDetails) : RealStateDetailsUIState()
+}
