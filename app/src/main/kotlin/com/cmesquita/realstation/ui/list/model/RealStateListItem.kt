144d747a743ec7742982ea76d82469d09160c024
Carlos Mesquita <carlosmesquita94@outlook.es>
2023-09-16 20:49:38 +0200

    Allow nullable photo URLs

--- app/src/main/kotlin/com/cmesquita/realstation/ui/list/model/RealStateListItem.kt
+++ app/src/main/kotlin/com/cmesquita/realstation/ui/list/model/RealStateListItem.kt
@@ -2,7 +2,7 @@ package com.cmesquita.realstation.ui.list.model
 
 data class RealStateListItem(
     val id: String,
-    val photoURL: String,
+    val photoURL: String?,
     val price: String,
     val location: String,
     val area: String,
