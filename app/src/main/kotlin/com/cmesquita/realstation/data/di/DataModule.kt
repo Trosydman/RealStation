067e0fee5ce90ab3d4849e8a24787b34d3564da4
Carlos Mesquita <carlosmesquita94@outlook.es>
2023-09-18 16:54:22 +0200

    Handle offline edge case

--- app/src/main/kotlin/com/cmesquita/realstation/data/di/DataModule.kt
+++ app/src/main/kotlin/com/cmesquita/realstation/data/di/DataModule.kt
@@ -1,15 +1,19 @@
 package com.cmesquita.realstation.data.di
 
+import android.content.Context
 import com.cmesquita.realstation.data.RealStateRepository
 import com.cmesquita.realstation.data.RealStateRepositoryImpl
 import com.cmesquita.realstation.data.remote.AvivAPI
+import com.cmesquita.realstation.data.remote.NetworkConnectionInterceptor
 import com.cmesquita.realstation.data.remote.model.RealStateDTOMapper
 import com.cmesquita.realstation.data.remote.model.RealStateListDTOMapper
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -32,10 +36,17 @@ interface DataModule {
 
         @Singleton
         @Provides
-        fun provideAvivAPI(): AvivAPI = Retrofit.Builder()
-            .baseUrl(AvivAPI.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(AvivAPI::class.java)
+        fun provideAvivAPI(@ApplicationContext context: Context): AvivAPI {
+            val httpClient: OkHttpClient = OkHttpClient.Builder()
+                .addInterceptor(NetworkConnectionInterceptor(context))
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(AvivAPI.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
+                .build()
+                .create(AvivAPI::class.java)
+        }
     }
 }
