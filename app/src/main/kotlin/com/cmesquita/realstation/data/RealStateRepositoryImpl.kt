ae6390ecddc44b6296f77f3789ac4a9679b3fe67
Carlos Mesquita <carlosmesquita94@outlook.es>
2023-09-18 16:47:57 +0200

    Handle NoConnectivityException

--- app/src/main/kotlin/com/cmesquita/realstation/data/RealStateRepositoryImpl.kt
+++ app/src/main/kotlin/com/cmesquita/realstation/data/RealStateRepositoryImpl.kt
@@ -2,12 +2,14 @@ package com.cmesquita.realstation.data
 
 import com.cmesquita.realstation.data.model.Result
 import com.cmesquita.realstation.data.remote.AvivAPI
+import com.cmesquita.realstation.data.remote.NoConnectivityException
 import com.cmesquita.realstation.data.remote.model.RealStateDTOMapper
 import com.cmesquita.realstation.data.remote.model.RealStateListDTOMapper
 import com.cmesquita.realstation.domain.model.RealState
 import retrofit2.Response
 import javax.inject.Inject
 
+private const val CODE_NOCONNECTIVITYEXCEPTION = -1
 private const val CODE_NULLPOINTEREXCEPTION = 0
 
 class RealStateRepositoryImpl @Inject constructor(
@@ -18,32 +20,40 @@ class RealStateRepositoryImpl @Inject constructor(
 
     override suspend fun getList(): Result<List<RealState>> {
         return handleResponse(
-            response = avivAPI.getRealStateList(),
-            transform = { realStateListDTOMapper.toDomain(it) }
+            response = { avivAPI.getRealStateList() },
+            transform = { realStateListDTOMapper.toDomain(it) },
         )
     }
 
     override suspend fun getBy(id: String): Result<RealState> {
         return handleResponse(
-            response = avivAPI.getRealStateBy(id),
-            transform = { realStateDTOMapper.toDomain(it) }
+            response = { avivAPI.getRealStateBy(id) },
+            transform = { realStateDTOMapper.toDomain(it) },
         )
     }
 
     @Suppress("TooGenericExceptionCaught")
-    private fun <S, T> handleResponse(response: Response<S>, transform: (S) -> T): Result<T> {
-        val responseBody = response.body()
+    private suspend fun <S, T> handleResponse(
+        response: suspend () -> Response<S>,
+        transform: (S) -> T
+    ): Result<T> {
+        try {
+            val responseValue = response()
+            val responseBody = responseValue.body()
 
-        return if (response.isSuccessful && responseBody != null) {
-            try {
-                Result.Success(transform(responseBody))
-            } catch (e: NullPointerException) {
-                Result.Error(code = CODE_NULLPOINTEREXCEPTION, message = e.localizedMessage)
+            return if (responseValue.isSuccessful && responseBody != null) {
+                try {
+                    Result.Success(transform(responseBody))
+                } catch (e: NullPointerException) {
+                    Result.Error(code = CODE_NULLPOINTEREXCEPTION, message = e.localizedMessage)
+                }
+            } else {
+                Result.Error(
+                    code = responseValue.code(), message = responseValue.errorBody()?.string()
+                )
             }
-        } else {
-            Result.Error(
-                code = response.code(), message = response.errorBody()?.string()
-            )
+        } catch (e: NoConnectivityException) {
+            return Result.Error(code = CODE_NOCONNECTIVITYEXCEPTION, message = e.localizedMessage)
         }
     }
 }
