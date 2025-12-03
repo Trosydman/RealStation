b05135290299fb2a092c168585afa5c793c81dc5
Carlos Mesquita <carlos.mesquita.cm2@gmail.com>
2025-12-03 18:35:11 +0100

    Remove unused kotlinCompilerExtensionVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,9 +58,6 @@ android {
         compose = true
         buildConfig = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.2"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
